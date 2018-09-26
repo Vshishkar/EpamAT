dd0745bcbf255550613dc517afc5b95d2283bc01
unknown <vshishkarov@gmail.com>
2018-09-26 09:08:05 +0300

    add StudentService documentation

--- lab_1/src/main/java/by/mmf/vshishkar/student/service/StudentService.java
+++ lab_1/src/main/java/by/mmf/vshishkar/student/service/StudentService.java
@@ -7,7 +7,8 @@ import java.util.List;
 public interface StudentService {
 
     double getAverageMark(Student student);
-
+    
+    /*this is my super commit */
     double getAverageMark(List<Student> students);
 
     List<Student> getStudentsByGroupname(List<Student> students,int groupName);
